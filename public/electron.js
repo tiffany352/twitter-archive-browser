bf198aa6cd41475221a9355299a9b743b17f337a
Tiffany Bennett <tiffnixen@gmail.com>
2019-10-08 19:41:53 -0700

    Replace some default strings

--- public/electron.js
+++ public/electron.js
@@ -1,39 +1,40 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 
-const path = require('path');
-const isDev = require('electron-is-dev');
+const path = require('path')
+const isDev = require('electron-is-dev')
 
-let mainWindow;
+let mainWindow
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 900, height: 680,
+    backgroundColor: '#15202b',
     webPreferences: {
       nodeIntegration: true,
       webSecurity: true
     }
-  });
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  })
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools()
   }
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.on('closed', () => mainWindow = null)
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { useSelector } from 'react-redux'
 import PromptArchive from './PromptArchive'
 import Session from './Session'
 import './App.css'
+
+function AppTitle(props) {
+  const accountName = useSelector((state) => state.session != null && state.session.account.username)
+
+  const title = (
+    <title>
+      {accountName ? `${accountName} - Twitter Archive Browser` : `Twitter Archive Browser`}
+    </title>
+  )
+  
+  return ReactDOM.createPortal(title, document.head)
+}
 
 export default function App(props) {
   const haveSession = useSelector((state) => state.session != null)
@@ -10,6 +23,7 @@ export default function App(props) {
   return (
     <div className="App">
       {haveSession ? <Session /> : <PromptArchive />}
+      <AppTitle />
     </div>
   )
 }
