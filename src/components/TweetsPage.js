341e2c4cd4785fa98160c91a82ca0dab615a7ee4
Tiffany Bennett <tiffnixen@gmail.com>
2019-10-13 22:11:07 -0700

    Implement a timeline view for tweet history

--- src/components/TweetsPage.js
+++ src/components/TweetsPage.js
@@ -4,6 +4,7 @@ import Tweet from './Tweet'
 import SessionContext from './SessionContext'
 import useQuery from '../useQuery'
 import MediaViewer from './MediaViewer'
+import './TweetsPage.css'
 
 export default function TweetsPage(props) {
   const [count, setCount] = useState(30)
@@ -14,6 +15,35 @@ export default function TweetsPage(props) {
   const search = query.get('search')
   const needle = search && search.toLowerCase()
   let hasMore = false
+
+  const oldest = new Date(session.account.createdDate)
+  let newest = new Date()
+  if (session.tweet.length > 0) {
+    newest = session.tweet[0].created_date
+  }
+  const tweetsPerDay = new Map()
+  for (const tweet of session.tweet) {
+    const date = tweet.created_date
+    const key = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+    tweetsPerDay.set(key, (tweetsPerDay.get(key) || 0) + 1)
+  }
+  oldest.setDate(oldest.getDate() - oldest.getDay())
+
+  const tweetsPerWeek = []
+  for (const date = new Date(oldest); date <= newest; date.setDate(date.getDate() + 7)) {
+    let sum = 0
+    for (let i = 0; i < 7; i++) {
+      const weekDay = new Date(date)
+      weekDay.setDate(weekDay.getDate() + i)
+      sum += tweetsPerDay.get(`${weekDay.getFullYear()}-${weekDay.getMonth()}-${weekDay.getDate()}`) || 0
+    }
+    tweetsPerWeek.push({
+      count: sum,
+      date: new Date(date),
+    })
+  }
+  tweetsPerWeek.reverse()
+  const maxCount = tweetsPerWeek.reduce((acc, { count }) => Math.max(acc, count), 0)
 
   for (const tweet of session.tweet) {
     if (props.includeReplies || !tweet.in_reply_to_user_id_str) {
@@ -34,11 +64,35 @@ export default function TweetsPage(props) {
   return (
     <>
       <aside className="Session-sidebar">
-        2019
-        2018
-        2017
-        2016
-        2015
+        {tweetsPerWeek.map(({ count, date }, index, array) => {
+          const showYear = index === 0 || (
+            date.getFullYear() !== array[index - 1].date.getFullYear()
+          )
+          const options = {
+            month: 'short',
+            day: 'numeric',
+          }
+          const startDate = date.toLocaleDateString(undefined, options)
+          const endOfWeek = new Date(date)
+          endOfWeek.setDate(endOfWeek.getDate() + 6)
+          const endDate = endOfWeek.toLocaleDateString(undefined, options)
+          return (
+            <React.Fragment key={index}>
+              {showYear && (
+                <h3>{date.getFullYear()}</h3>
+              )}
+              <div
+                className="TweetsPage-week"
+                data-date={`${startDate} - ${endDate} (${count} tweets)`}
+              >
+                <div
+                  className="TweetsPage-weekCount"
+                  style={{ width: `${count / maxCount * 98 + 2}%` }}
+                />
+              </div>
+            </React.Fragment>
+          )
+        })}
       </aside>
       <MediaViewer />
       <article className="Session-content">
